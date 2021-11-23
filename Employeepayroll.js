92933634ce745dd4cacd0ed191639e05fea6f7b9
pitu <ashishkumarvish.432@gmail.com>
2021-11-24 00:30:18 +0530

    Added:Checking employee is present or absent

--- Employeepayroll.js
+++ Employeepayroll.js
@@ -1,1 +1,9 @@
-console.log("Welcome to EmployeePayroll Program")
+ // console.log("Welcome to EmployeePayroll Program")
+const CHECK_EMPLOYEE_ABSENT = 0;
+let employee_check = Math.floor(Math.random() * 10) % 2;
+if (employee_check == CHECK_EMPLOYEE_ABSENT) {
+    console.log("Employee is absent...");
+    return;
+}
+else
+    console.log("Employee is present...");
